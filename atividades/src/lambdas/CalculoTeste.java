8d0ae1235bada281dd2c2280cdac0f8976d2d608
Fábio Braz <fabiobrazferreira@gmail.com>
2022-11-25 11:36:56 -0300

    Atualização de Código

--- atividades/src/lambdas/CalculoTeste.java
+++ atividades/src/lambdas/CalculoTeste.java
@@ -3,9 +3,37 @@ package lambdas;
 public class CalculoTeste {
 	public static void main(String[] args) {
 		
+		System.out.println("VALORES USADOS: 14+15, 14+15, 14*15");
+
+		System.out.println("CLASSE P.O.O");
+		
+		// PADRÃO USANDO P.O.O
 		iCalculo somar = new Soma();
 		iCalculo multi = new Multiplicar();
 		
-		System.out.println(somar.executar(10,20)-multi.executar(2, 4));
+		System.out.println(somar.executar(14,15));
+		
+		
+		System.out.println("FUNÇÃO LAMBDA");
+		
+		// PADRÃO USANDO FUNÇÕES LAMBDAS
+		
+		// AQUI EU CRIEI UM OBJETO, "QUE NA TEÓRIA DEVERIA TER UMA CLASSE SÓ PRA CRIAR ESSE MÉTODO"
+		iCalculo soma = (x, y) -> {
+			return x + y; 
+		};
+		
+		System.out.println(soma.executar(14, 15));
+		
+		System.out.println("FUNÇÃO LAMBDA SUBESCREVIDA");
+		
+		// EU SUBESCREVEI A FUNÇÃO LAMBDA SAMA CRIADA USANDO A INTERFACE LOGO ACIMA. 
+		soma = (x, y) -> {
+			return x * y; 
+		};
+		
+		System.out.println(soma.executar(14, 15));
+		
+		
 	}
 }
